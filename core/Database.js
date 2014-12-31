ae41997bdb3ddd2627c587049a7f5c3a7dd0f9e9
djchrisnet <github@djchrisnet.de>
2014-12-31 16:56:20 +0100

    Adding nice save und load functions

--- core/Database.js
+++ core/Database.js
@@ -6,4 +6,68 @@ var DB	= (new function() {
 	this.getChannel = function() {
 		return KnuddelsServer.getPersistence();
 	};
+	
+	this.load = function(key, defaultValue, user) {
+		if(key === undefined) {
+			Logger.error("No key submitted");
+			return false;
+		}
+		
+		selectedDB = this.getChannel();
+		if(user != undefined) { 
+			selectedDB = this.getUser(user);
+		}
+		
+		switch(typeof defaultValue) {
+			case 'string':
+				return selectedDB.getString(key, defaultValue);
+			break;	
+
+			case 'number':
+				return selectedDB.getNumber(key, defaultValue);
+			break;
+			
+			case 'object':
+			case 'undefined':			
+				return selectedDB.getObject(key, defaultValue);
+			break;
+		}
+		
+		return false;
+	};
+
+	this.save = function(key, data, user) {
+		if(key === undefined) {
+			Logger.error("No key submitted");
+			return false;
+		}
+
+		if(data === undefined) {
+			Logger.error("No Data submitted");
+			return false;
+		}
+		
+		selectedDB = this.getChannel();
+		if(user != undefined) { 
+			selectedDB = this.getUser(user);
+		}
+		
+		switch(typeof data) {
+			case 'string':
+				selectedDB.setString(key, data);
+			break;	
+
+			case 'number':
+				selectedDB.setNumber(key, data);
+			break;
+			
+			case 'object':
+				selectedDB.getObject(key, data);
+			break;
+		}
+		return true;
+	};
+	
+	Logger.info("This shit is _DEPERECATED_!");
+	return this.getChannel();
 }());
