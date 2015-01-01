59ced2f0368421648d93e550c6970347102b9427
djchrisnet <github@djchrisnet.de>
2015-01-01 02:54:11 +0100

    Oh bitte hau mich nicht :-D

--- core/Cronjob.js
+++ core/Cronjob.js
@@ -11,12 +11,25 @@ function Cronjob(name, cycle, callback) {
 		_cycle		= cycle;
 		_callback	= callback;
 		_cycle_data = _cycle.match(/^([0-9]+|\*{1})[ \n\t\b]+([0-9]+|\*{1})[ \n\t\b]+([0-9]+|\*{1})[ \n\t\b]+([0-9]+|\*{1})[ \n\t\b]+([0-9]+|\*{1})[ \n\t\b]*$/);
-		_last_run	= new Date(DB.load('_cronjob_' + _name, undefined));
+		db = DB.load('_cronjob_' + _name, {run:undefined, check:undefined});
+		_last_run	= new Date(db.run);
+		_last_check	= new Date(db.check);		
 		
-		/*if(new Date().getTime() < _last_run.getTime()) {
+		
+		if(new Date().getTime() < _last_check.getTime()) {
+			
+			while(true) {
+				time = new Date(_last_check.getTime()+500);
+				if(this.check(time)) {
+					_last_run = time
+					_callback(_last_run);
+					break;
+				}
+			}
+			
 			_last_run = new Date();
 			_callback(_last_run);
-		}*/
+		}
 		
 		instance.start();
 	}
@@ -32,13 +45,14 @@ function Cronjob(name, cycle, callback) {
 		}
 	};
 	
-	this.run = function() {
-		var time	= new Date();
+	this.check = function(time) {
 		var day		= _cycle_data[5] || '*';
 		var month	= _cycle_data[4] || '*';
 		var year	= _cycle_data[3] || '*';
 		var minute	= _cycle_data[1] || '*';
 		var hour	= _cycle_data[2] || '*';
+
+		_last_check = time;
 		
 		if(time.getTime() - _last_run.getTime() > 60000) {
 			if(minute == '*' || parseInt(minute) == time.getMinutes()) {
@@ -46,19 +60,29 @@ function Cronjob(name, cycle, callback) {
 					if(day == '*' || parseInt(day) == time.getDate()) {
 						if(month == '*' || (parseInt(month) - 1) == time.getMonth()) {
 							if(day == '*' || parseInt(day) == time.getDay()) {
-								_last_run = time;
-								_callback(time);
+								return true;
 							}
 						}
 					}
 				}
 			}
 		}
+		return false;		
+	};
+	
+	this.run = function() {
+		var time	= new Date();
+		
+		if(this.check(time)) {
+			_last_run = time;
+			_callback(time);
+		}
+		
+		_last_check = time;
 	};
 	
 	this.onShutdown = function() {
-		//DB.deleteString('_cronjob_' + _name);
-		DB.save('_cronjob_' + _name, _last_run.toGMTString());
+		DB.save('_cronjob_' + _name, {run:_last_run.toGMTString(), check:_last_check.toGMTString()});
 	};
 	
 	Cronjob(this, name, cycle, callback);
