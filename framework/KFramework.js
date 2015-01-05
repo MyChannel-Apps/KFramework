1c153517cd9819cf21f841c914522a67b217926b
Adrian Preuß <info@adi-code.de>
2015-01-05 06:52:07 +0100

    Add Constants & Channel

--- framework/KFramework.js
+++ framework/KFramework.js
@@ -26,16 +26,20 @@
 
 var VERSION		= '1.0.3';
 
-//Tools
+// Defaults
+require('framework/Constants.js');
+
+// Tools
 require('framework/tools/String.js');
 require('framework/tools/Object.js');
 require('framework/tools/Array.js');
 
-//Core
+// Core
 require('framework/core/Logger.js');
 require('framework/core/Database.js');
 require('framework/core/Cronjob.js');
 require('framework/core/Bot.js');
 require('framework/core/KCode.js');
 require('framework/core/KBank.js');
+require('framework/core/Channel.js');
 require('framework/core/User.js');
