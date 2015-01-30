f58a3f46a15b35857bbe3d58f0cb25f198ab6ec3
Adrian Preuß <info@adi-code.de>
2015-01-30 18:59:24 +0100

    Tools/String: Remove startsWith + endsWith #4

--- framework/tools/String.js
+++ framework/tools/String.js
@@ -24,34 +24,6 @@
 	@author		Christoph Kühl <djchrisnet>
 */
 
-if(!String.prototype.startsWith) {
-	Object.defineProperty(String.prototype, 'startsWith', {
-		enumerable:		false,
-		configurable:	false,
-		writable:		false,
-		value:			function(searchString, position) {
-			position = position || 0;
-			return this.lastIndexOf(searchString, position) === position;
-		}
-	});
-}
-
-if(!String.prototype.endsWith) {
-	Object.defineProperty(String.prototype, 'endsWith', {
-		value: function(searchString, position) {
-			var subjectString = this.toString();
-			
-			if(position === undefined || position > subjectString.length) {
-				position = subjectString.length;
-			}
-			
-			position -= searchString.length;
-			var lastIndex = subjectString.indexOf(searchString, position);
-			return lastIndex !== -1 && lastIndex === position;
-		}
-	});
-}
-
 if(!String.prototype.format) {
 	Object.defineProperty(String.prototype, 'format', {
 		enumerable:		false,
