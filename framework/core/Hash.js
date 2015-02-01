17e28219d82fe4afdbb3865812c279a03cbb7c1c
Adrian Preuß <info@adi-code.de>
2015-02-01 05:39:37 +0100

    Hash: Add warning on comment

--- framework/core/Hash.js
+++ framework/core/Hash.js
@@ -28,8 +28,11 @@ var Hash = {
 	decodeForm: function(string) {
 		var chars	= string.split('');
 		var output	= [];
+		
+		/* #### WARNING: this variables will be updated each client update! #### */
 		var a		= 97;
 		var b		= 4;
+		/* #### WARNING: this variables will be updated each client update! #### */
 		
 		for(var index = 0, char_index = 0; index < chars.length / 2; index++, char_index += 2) {
 			output.push((chars[char_index].charCodeAt(0) - a << b | chars[char_index + 1].charCodeAt(0) - a) ^ index & 255);
