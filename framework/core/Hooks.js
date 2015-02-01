0a29275cba9b8814361a3e7313c9b13478cdc655
Adrian Preuß <info@adi-code.de>
2015-02-01 05:58:59 +0100

    Hooks: prepare for single usage

--- framework/core/Hooks.js
+++ framework/core/Hooks.js
@@ -45,7 +45,11 @@ var Hooks = (new function() {
 		}
 		
 		if(_debug) {
-			Logger.info('[Hooks] Adding "' + name + '" with Priority of ' + priority);
+			if(Logger == undefined) {
+				KnuddelsServer.getDefaultLogger().info('[Hooks] Adding "' + name + '" with Priority of ' + priority);
+			} else {
+				Logger.info('[Hooks] Adding "' + name + '" with Priority of ' + priority);
+			}
 		}
 		
 		_hooks[priority].push({
@@ -59,14 +63,28 @@ var Hooks = (new function() {
 		priority = (priority == undefined ? 10 : priority); // default Priority is 10
 		
 		if(_debug) {
-			Logger.info('[Hooks] Removing "' + name + '" with Priority of ' + priority);
+			if(Logger == undefined) {
+				KnuddelsServer.getDefaultLogger().info('[Hooks] Removing "' + name + '" with Priority of ' + priority);
+			} else {
+				Logger.info('[Hooks] Removing "' + name + '" with Priority of ' + priority);
+			}
 		}
 		
-		_hooks[priority].each(function(hook, index) {
-			if(hook.name == name) {
-				delete _hooks[priority][index];
+		if(!Object.prototype.each) {
+			for(var index in _hooks[priority]) {
+				var hook = _hooks[priority][index];
+				
+				if(hook.name == name) {
+					delete _hooks[priority][index];
+				}
 			}
-		});
+		} else {
+			_hooks[priority].each(function(hook, index) {
+				if(hook.name == name) {
+					delete _hooks[priority][index];
+				}
+			});
+		}
 	};
 	
 	this.applyFilter = function(name) {
@@ -90,7 +108,11 @@ var Hooks = (new function() {
 	
 	this.do = function(name, is_filter) {
 		if(_debug) {
-			Logger.info('[Hooks] ' + JSON.stringify(arguments));
+			if(Logger == undefined) {
+				KnuddelsServer.getDefaultLogger().info('[Hooks] ' + JSON.stringify(arguments));
+			} else {
+				Logger.info('[Hooks] ' + JSON.stringify(arguments));
+			}
 		}
 		
 		var is_filter_set	= (is_filter == undefined || typeof(is_filter) != 'boolean');
@@ -118,27 +140,69 @@ var Hooks = (new function() {
 		output			= args[0];
 		
 		if(_debug) {
-			Logger.info('[Hooks] Execute "' + name + '" with params: ' + JSON.stringify(args));
+			if(Logger == undefined) {
+				KnuddelsServer.getDefaultLogger().info('[Hooks] Execute "' + name + '" with params: ' + JSON.stringify(args));
+			} else {
+				Logger.info('[Hooks] Execute "' + name + '" with params: ' + JSON.stringify(args));
+			}
 		}
 		
 		// sort by priority
-		_hooks.sort('index', 'ASC');
-		
-		_hooks.each(function(hooks, priority) {
-			if(_debug) {
-				Logger.info('[Hooks] Each: PRIORITY ' + priority);
+		if(!Array.prototype.sort) {
+			if(Logger == undefined) {
+				KnuddelsServer.getDefaultLogger().warn('Hooks won\'t sort by Priority because Array.sort(); is not available. Please import framework/tools/Array.js!');
+			} else {
+				Logger.warn('Hooks won\'t sort by Priority because Array.sort(); is not available. Please import framework/tools/Array.js!');
 			}
-			
-			hooks.each(function(hook) {
-				if(hook.name == name) {
-					if(typeof(output) != 'array') {
-						output = [ output ];
+		} else {
+			_hooks.sort('index', 'ASC');
+		}
+		
+		if(!Object.prototype.each) {
+			for(var priority in _hooks) {
+				var hooks = _hooks[priority];
+				
+				if(_debug) {
+					if(Logger == undefined) {
+						KnuddelsServer.getDefaultLogger().info('[Hooks] Each: PRIORITY ' + priority);
+					} else {
+						Logger.info('[Hooks] Each: PRIORITY ' + priority);
 					}
-					
-					output = hook.callback.apply(this, ((is_filter_set ? false : is_filter) ? output : args));
 				}
+				
+				for(var index in hooks) {
+					var hook = hooks[index];
+					
+					if(hook.name == name) {
+						if(typeof(output) != 'array') {
+							output = [ output ];
+						}
+						
+						output = hook.callback.apply(this, ((is_filter_set ? false : is_filter) ? output : args));
+					}
+				});
+			}
+		} else {
+			_hooks.each(function(hooks, priority) {
+				if(_debug) {
+					if(Logger == undefined) {
+						KnuddelsServer.getDefaultLogger().info('[Hooks] Each: PRIORITY ' + priority);
+					} else {
+						Logger.info('[Hooks] Each: PRIORITY ' + priority);
+					}
+				}
+				
+				hooks.each(function(hook) {
+					if(hook.name == name) {
+						if(typeof(output) != 'array') {
+							output = [ output ];
+						}
+						
+						output = hook.callback.apply(this, ((is_filter_set ? false : is_filter) ? output : args));
+					}
+				});
 			});
-		});
+		}
 		
 		return output;
 	};
