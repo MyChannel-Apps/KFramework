5e927c4af75f068f69ef683e047f7884fa40a0a0
Adrian Preuß <info@adi-code.de>
2015-02-20 17:14:42 +0100

    User: adding Docs/Copyright

--- framework/tools/User.js
+++ framework/tools/User.js
@@ -1,6 +1,50 @@
-var User = User || {};
-User.prototype = User.prototype || {};
+/**
+	The MIT License (MIT)
 
+	Copyright (c) 2014 MyChannel-Apps.de
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+	
+	@author		Christoph Kühl <djchrisnet>, Adrian Preuß <Bizarrus>
+	@docs		http://www.mychannel-apps.de/documentation/tools/user
+*/
+
+var User		= User || {};
+User.prototype	= User.prototype || {};
+
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_getID
+*/
+if(!User.prototype.private) {
+	Object.defineProperty(User.prototype, 'getID', {
+		enumerable:		false,
+		configurable:	false,
+		writable:		false,
+		value: function() {
+			return this.getUserId();
+		}
+	});
+}
+
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_private
+*/
 if(!User.prototype.private) {
 	Object.defineProperty(User.prototype, 'private', {
 		enumerable:		false,
@@ -12,6 +56,9 @@ if(!User.prototype.private) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_post
+*/
 if(!User.prototype.post) {
 	Object.defineProperty(User.prototype, 'post', {
 		enumerable:		false,
@@ -23,6 +70,9 @@ if(!User.prototype.post) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_getProfilePicture
+*/
 if(!User.prototype.getProfilePicture) {
 	Object.defineProperty(User.prototype, 'getProfilePicture', {
 		enumerable:		false,
@@ -34,6 +84,9 @@ if(!User.prototype.getProfilePicture) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_getKonto
+*/
 if(!User.prototype.getKonto) {
 	Object.defineProperty(User.prototype, 'getKonto', {
 		enumerable:		false,
@@ -45,6 +98,9 @@ if(!User.prototype.getKonto) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_getKn
+*/
 if(!User.prototype.getKn) {
 	Object.defineProperty(User.prototype, 'getKn', {
 		enumerable:		false,
@@ -56,6 +112,9 @@ if(!User.prototype.getKn) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_addKn
+*/
 if(!User.prototype.addKn) {
 	Object.defineProperty(User.prototype, 'addKn', {
 		enumerable:		false,
@@ -67,6 +126,9 @@ if(!User.prototype.addKn) {
 	});
 }
 
+/*
+	@docs	http://www.mychannel-apps.de/documentation/User_subKn
+*/
 if(!User.prototype.subKn) {
 	Object.defineProperty(User.prototype, 'subKn', {
 		enumerable:		false,
